3f3a390c11ecda0865c205847160c7aac852fbe4
Aashnan <aashnangoyal12@gmail.com>
2024-03-03 07:22:51 +0530

    This is week 3

---
+++ Week3/SelectionSort.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void SelectionSort(vector<int> &arr,int n){
+    int comp = 0;
+    int swaps = 0;
+    for(int i=0;i<n;i++){
+        int pos = i;
+        for(int j=i+1;j<n;j++){
+            comp++;
+            if(arr[pos] > arr[j]){
+                pos = j;
+            }
+        }
+        if(pos != i){
+            swap(arr[pos],arr[i]);
+            swaps++;
+        }
+    }
+    cout<<"Comparisions : "<<comp<<endl;
+    cout<<"Swaps : "<<swaps<<endl;
+}
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        cout<<"Before Sort"<<endl;
+        for(int i:arr){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        SelectionSort(arr,n);
+        for(int i:arr){
+            cout<<i<<" ";
+        }
+    }
+}
