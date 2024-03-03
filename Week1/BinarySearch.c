928d8a443618949baa32114da05a8859e81e69e5
Aashnan <aashnangoyal12@gmail.com>
2024-03-03 06:42:19 +0530

    This is week 1

--- Week1/BinarySearch.c
+++ Week1/BinarySearch.c
@@ -4,13 +4,14 @@ int BinarySearch(int arr[],int n,int key){
     int start = 0;
     int end = n-1;
 
+    //Binary Search
     while(start <= end){
         int mid = start + (end-start)/2;
         if(arr[mid] == key)return mid;
         else if(arr[mid] > key)end = mid-1;
         else start = mid+1;
     }
-    return -1;
+    return -101;
 }
 
 int main() {
@@ -26,7 +27,7 @@ int main() {
         int key;
         scanf("%d",&key);
         int pos = BinarySearch(arr,n,key);
-        if(pos != -1){
+        if(pos != -101){
             printf("Present : %d\n",pos+1);
         }
         else {
